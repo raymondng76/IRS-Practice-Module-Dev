de4617ec9b61aaea0cd11e13b3450f70fb8be1b8
yoke2 <wongyokekeong@gmail.com>
2020-02-16 21:23:08 +0800

    base version of airsim env and test file

---
+++ code/StraightTrack.py
@@ -0,0 +1,63 @@
+# Based on: https://github.com/sunghoonhong/AirsimDRL/blob/master/randomly.py
+# Test Script that only moves forward
+
+import time
+import csv
+import math
+import argparse
+import numpy as np
+from airsim_env import Env
+
+class StraightAgentDiscrete(object):
+    def __init__(self, action_size):
+        self.action_size = action_size
+
+    def get_action(self):
+        action = 1 # Always go straight for testing
+        return action
+    
+def interpret_action(action):
+    scaling_factor = 0.5
+    if action == 0:
+        quad_offset = (0, 0, 0)
+    elif action == 1:
+        quad_offset = (scaling_factor, 0, 0)
+    elif action == 2:
+        quad_offset = (0, scaling_factor, 0)
+    elif action == 3:
+        quad_offset = (0, 0, scaling_factor)
+    elif action == 4:
+        quad_offset = (-scaling_factor, 0, 0)    
+    elif action == 5:
+        quad_offset = (0, -scaling_factor, 0)
+    elif action == 6:
+        quad_offset = (0, 0, -scaling_factor)
+    
+    return quad_offset
+
+agent = StraightAgentDiscrete(1)
+
+env = Env()
+episode = 0
+while True:
+    done = False
+    timestep = 0
+    score = 0
+    _ = env.reset()
+    print('env reset!')
+    
+    while not done:
+        print('start episode')
+        timestep += 1
+        action = agent.get_action()
+        action = interpret_action(action)
+        _, reward, done, info = env.step(action)
+        score += reward
+
+        # stack history here
+        print('Step %d Action %s Reward %.2f Info %s:' % (timestep, action, reward, info))
+    # done
+    print('Ep %d: Step %d Score %.3f' % (episode, timestep, score))
+    episode += 1
+    
+#env.disconnect()
