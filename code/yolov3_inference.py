b2e092fd717ca5889205e8eb33f553cf204e2340
Raymond <35454616+raymondng76@users.noreply.github.com>
2020-03-07 14:34:22 +0800

    Update yolov3_inference.py

--- code/yolov3_inference.py
+++ code/yolov3_inference.py
@@ -1,3 +1,12 @@
+"""
+Date: 1/2/2020
+Team: Kenneth Goh (A0198544N) Raymond Ng (A0198543R) Wong Yoke Keong (A0195365U)
+
+Intelligent Robotic Systems Practice Module
+
+Code is adapted from:
+https://github.com/experiencor/keras-yolo3
+"""
 import cv2
 import argparse
 import numpy as np
@@ -189,39 +198,7 @@ class YoloPredictor:
     def softmax(self, x, axis=-1):
         x = x - np.amax(x, axis, keepdims=True)
         e_x = np.exp(x)
-        return e_x / e_x.sum(axis, keepdims=True)
-
-# def draw_boxes(image, boxes, labels, obj_thresh, quiet=True):
-#     for box in boxes:
-#         label_str = ''
-#         label = -1
-        
-#         for i in range(len(labels)):
-#             if box.classes[i] > obj_thresh:
-#                 if label_str != '': label_str += ', '
-#                 label_str += (labels[i] + ' ' + str(round(box.get_score()*100, 2)) + '%')
-#                 label = i
-#             if not quiet: print(label_str)
-                
-#         if label >= 0:
-#             text_size = cv2.getTextSize(label_str, cv2.FONT_HERSHEY_SIMPLEX, 1.1e-3 * image.shape[0], 1)
-#             width, height = text_size[0][0], text_size[0][1]
-#             region = np.array([[box.xmin-3,        box.ymin], 
-#                                [box.xmin-3,        box.ymin-height-26], 
-#                                [box.xmin+width+13, box.ymin-height-26], 
-#                                [box.xmin+width+13, box.ymin]], dtype='int32')  
-
-#             cv2.rectangle(img=image, pt1=(box.xmin,box.ymin), pt2=(box.xmax,box.ymax), color=get_color(label), thickness=1)
-#             cv2.fillPoly(img=image, pts=[region], color=get_color(label))
-#             cv2.putText(img=image, 
-#                         text=label_str, 
-#                         org=(box.xmin+13, box.ymin - 13), 
-#                         fontFace=cv2.FONT_HERSHEY_SIMPLEX, 
-#                         fontScale=1e-3 * image.shape[0], 
-#                         color=(0,0,0), 
-#                         thickness=1)
-        
-#     return image         
+        return e_x / e_x.sum(axis, keepdims=True)         
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
